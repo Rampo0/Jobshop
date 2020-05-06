23f8f2fce38bd705c84f871702e12088b5fae5b2
Ammar Dwi Anwari <weissman1505199@gmail.com>
2020-05-06 21:44:48 +0700

    upload pekerjaan feature

--- app/modules/order/presentation/controllers/IndexController.php
+++ app/modules/order/presentation/controllers/IndexController.php
@@ -16,7 +16,35 @@ class IndexController extends ControllerBase
         return $orders;
     }
 
+    public function uploadAction(){
+
+        if(!$this->security->checkToken()){
+            echo "invalid csrf !!";
+        }
+
+        $order = Orders::findFirst($this->request->getPost('order_id'));
+
+        $file_name = "";
+        if ($this->request->hasFiles() == true) {
+            foreach ($this->request->getUploadedFiles() as $file) {
+                $file->moveTo('files/order/' . $file->getName());
+                $file_name = $file->getName();   
+            }
+        }
+
+        $order->uploaded_file = "order/" . $file_name;
+        $order->is_done = 1;
+        $order->save();
+
+        return $this->response->redirect("/order");
+    }
+
     public function createAction(){
+
+        if(!$this->security->checkToken()){
+            echo "invalid csrf !!";
+        }
+
         $user_id = $this->getDI()->getShared("session")->get('user_id');
         $post_id = $this->request->getPost('post_id');
 
